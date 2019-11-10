bfbc98d4b5ced2bb83e8230ec11dcf69d5898cd3
elenagrash <12345>
2019-11-09 18:16:04 -0600

    method added

---
+++ src/AppMonsterProject.java
@@ -0,0 +1,18 @@
+public class AppMonsterProject {
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if(aSmile&&bSmile){
+            return true;
+        }
+        if(!aSmile&&!bSmile){
+            return true;
+        }
+        return false;
+    }
+
+
+}
